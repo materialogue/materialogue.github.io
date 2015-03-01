1a7e7c691a22569b45d4ba642e59e3012b8b9396
Eachan Johnson <eachan.johnson@gmail.com>
2015-03-01 17:38:43 -0500

    Complete redesign

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
+////Menu items
 
+// Add hover callback
+function menuHover(menuId) {
+
+    var $menuItem = $('#' + menuId),
+        $menuItems = $('.menu-item'),
+        $menuTarget = $('#' + menuId.split('a-')[1]),
+        $sections = $('.col2');
+
+    if ( $menuTarget ) {
+        //console.log(menuTarget);
+        $menuItems.css({
+            'border-left': '4px solid transparent',
+            'padding-left': '0px'
+        });
+        $menuItem.css({
+            'border-left': '4px solid #000000',
+            'padding-left': '2.1%'
+        });
+        $sections.hide();
+        $menuTarget.fadeIn('fast');
+    } else {
+        //
+    }
+
+}
 
 
 //// Main function - stringing it all together!
 function main() {
-    ///
+    $('.menu-item').mouseenter( function () {
+        menuHover(this.id);
+    });
 }
 
 //// Make sure to wait for the page to load before executing
-$(document).load(main);
+$(document).ready(main);
