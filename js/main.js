333242f3a4c540e2b2b34d279f1a7b94bfce54f9
Eachan Johnson <eachan.johnson@gmail.com>
2015-09-05 20:48:34 -0400

    mobile-first redesign

--- js/main.js
+++ js/main.js
@@ -1,89 +1,49 @@
-//// Front cover reveal
+// Materialogue JS
 
-// Cover callback
-function coverCallback2(){
-    var $cover = $('.cover');
-
-    $cover.animate({
-        'top': '100%',
-        'opacity': 0
-    }, 'slow');
-    setTimeout(function () {
-        $cover.hide();
-    }, 1000);
+function revealMenu () {
+    var $menu = $('#menu'),
+        $menuBurger = $('#menu-burger');
+    
+    $menu.toggleClass('active');
+    $menuBurger.toggleClass('active');
+    
+    return $menu;
 }
 
-function coverCallback1(){
-    var $cover = $('.cover'),
-        $titleBar = $('.page-title');
-
-    var dropHeight = $titleBar.height() + 2;
-
-    $cover.animate({
-        'top': dropHeight
-    }, 'fast');
-
-    setTimeout(coverCallback2, 1000);
+function menuHandler () {
+    var $menuBurger = $('#menu-burger'),
+        $menuTitle = $('#menu h1');
+    
+    $menuBurger.click(revealMenu);
+    $menuTitle.click(revealMenu);
 }
 
-// Reveal page
-function coverReveal() {
-    var $cover = $('.cover'),
-        titleHeight = $('.page-title').height();
-
-    $cover.css('top', titleHeight);
-    $cover.show();
-    setTimeout(coverCallback1, 1000);
-}
-
-////Menu items
-
-// Add hover callback
-function menuHover(menuId) {
-
-    var $menuItem = $('#' + menuId),
-        $menuItems = $('.menu-item'),
-        $menuTarget = $('#' + menuId.split('a-')[1]),
-        $sections = $('.span-9'),
-        $currentDisplay = $('.current-display');
-
-    //console.log('#' + menuId.split('a-')[1])
-    //console.log($currentDisplay);
-
-    if ( $menuTarget ) {
-        //console.log(menuTarget);
-        $menuItems.css({
-            'border-left': '4px solid transparent',
-            'padding-left': '0px'
-        });
-        $menuItem.css({
-            'border-left': '4px solid #000000',
-            'padding-left': '2.1%'
-        });
-
-        if ( $menuTarget.hasClass('current-display') ) {
-            //
+function logoShifter () {
+    var $logo = $('#logo'),
+        $menuBurger = $('#menu-burger');
+    
+    $(document).on('scroll', function () {
+        var scrollTop = $('body').scrollTop(),
+            viewPortHeight = $(window).innerHeight();
+        
+        console.log('Detected scroll: scrollTop is ' + scrollTop + ' and viewport height is ' + viewPortHeight);
+        
+        if ( scrollTop > 0.5 * 0.9167 * viewPortHeight ) {
+            $logo.addClass('top');
+            $menuBurger.addClass('visible');
         } else {
-            $currentDisplay.removeClass('current-display');
-            $sections.hide();
-            $menuTarget.fadeIn('fast');
-            $menuTarget.addClass('current-display');
+            $logo.removeClass('top');
+            $menuBurger.removeClass('visible');
         }
-    } else {
-        //
-    }
-
-}
-
-
-//// Main function - stringing it all together!
-function main() {
-    coverReveal();
-    menuHover('a-sci-stories');
-    $('.menu-item').mouseenter( function () {
-        menuHover(this.id);
     });
 }
 
-//// Make sure to wait for the page to load before executing
-$(document).ready(main);
+
+function main () {
+    logoShifter();
+    menuHandler();
+}
+
+$(document).ready(function () {
+    main();
+});
