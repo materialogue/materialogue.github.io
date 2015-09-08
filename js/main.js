0c920e421c057c29ad521b1eff692b38e68b7651
Eachan Johnson <eachan.johnson@gmail.com>
2015-09-07 22:42:09 -0400

    replaced video, used color scheme, allowed navigation links, 100% more polished

--- js/main.js
+++ js/main.js
@@ -11,7 +11,19 @@ function revealMenu () {
 }
 
 function menuHandler () {
-    var $menuBurger = $('#menu-burger');
+    var $menu = $('#menu'),
+        $menuBurger = $('#menu-burger');
+    
+    $(document).on('scroll', function () {
+        var scrollTop = $('body').scrollTop(),
+            windowHeight = $(window).innerHeight();
+        
+        if ( scrollTop >= 0.5 * 0.9167 * windowHeight && ! $menu.hasClass('margin') ) {
+            $menu.addClass('margin');
+        } else if ( scrollTop < 0.5 * 0.9167 * windowHeight && $menu.hasClass('margin') ) {
+            $menu.removeClass('margin');
+        }
+    });
     
     $menuBurger.click(revealMenu);
 }
@@ -39,6 +51,8 @@ function logoShifter () {
 function smoothScroll (current, target, speed) {
     //console.log('Current scrolltop is ' + current + ' and target is ' + target);
     
+    var speed = speed;
+    
     if ( current + speed < target ) {
         $('body').scrollTop(current + speed);
     } else if ( current - speed > target ) {
@@ -64,7 +78,7 @@ function internalLinkHandler () {
         
         revealMenu();
         
-        smoothScroll(currentScrollTop, linkTargetOffset, Math.abs(currentScrollTop - linkTargetOffset) / (24 * 1.0));
+        smoothScroll(currentScrollTop, linkTargetOffset, Math.abs(currentScrollTop - linkTargetOffset) / (24 * 1/2));
     });
 }
 
@@ -79,23 +93,67 @@ function vidHandler () {
         var viewportHeight = $(window).innerHeight(),
             $body = $('body');
         
-        if ( $body.scrollTop() < viewportHeight ) {
-            setTimeout(function () {
-                window.requestAnimationFrame( function () {
-                    smoothScroll($body.scrollTop(), viewportHeight, viewportHeight / (24 * 1.5));
-                });
-            }, 1000);
+        if ( $body.scrollTop() < 20 ) {
+                smoothScroll($body.scrollTop(), viewportHeight, viewportHeight / (24 * 1.5));
         } else {
             //
+        }
+        
+        $vid.addEventListener('click', function () {
+            $vid.currentTime = 0;
+            $vid.play();
+        });
+    });
+}
+
+function navHighlighter () {
+    var $menuItems = $('#menu ul li'),
+        windowHeight = $(window).innerHeight();
+    
+    var lastVisibleId = 'cover-page',
+        nextVisibleId = '';
+    
+    $(document).scroll(function () {
+        
+        $menuItems.each(function (n, o) {
+            var menuTargetId = o.getAttribute('id').split('#a-')[1],
+                $menuTarget = $('#' + menuTargetId),
+                windowTop = $('body').scrollTop() - 0.3 * windowHeight,
+                windowBottom = windowTop + 1.3 * windowHeight;
+            
+            if ( $menuTarget.offset().top > windowTop && $menuTarget.offset().top < windowBottom )  {
+                nextVisibleId = menuTargetId;
+            } else {
+                //
+            }            
+        });
+        
+        if ( nextVisibleId != lastVisibleId ) {
+            var itemToHideId = '#a-' + lastVisibleId,
+                itemToShowId = '#a-' + nextVisibleId,
+                $itemToHide = document.getElementById(itemToHideId),
+                $itemToShow = document.getElementById(itemToShowId);
+            
+            console.log('Highlighting ' + itemToShowId + ' and lowlighting ' + itemToHideId);
+            
+            $('li.viewing').removeClass('viewing');
+            $itemToShow.setAttribute('class', 'viewing');
+            lastVisibleId = nextVisibleId;
+        } else {
+            console.log('No change in section: no change in menu highlighting.')
         }
     });
 }
 
 function main () {
+//    setTimeout(function () {
+//        $('body').fadeIn('slow');
+//    }, 3000);
     logoShifter();
     menuHandler();
     vidHandler();
     internalLinkHandler();
+    navHighlighter();
 }
 
 $(document).ready(function () {
